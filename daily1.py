ae994cb65e852ce37435f2d8b989134793015e8f
IgnacioMPalleiro <146377327+IgnacioMPalleiro@users.noreply.github.com>
2024-12-02 15:15:53 -0300

    Add files via upload

---
+++ daily1.py
@@ -0,0 +1,50 @@
+"""rating = input(("ingrese su rating "))
+rating = (float(rating))
+if rating > 4.5:
+    print('Extraordinary')
+
+elif rating > 4:
+    print('Excellent')
+
+elif rating > 3:
+    print('Good')
+
+elif rating > 2:
+    print('Fair')
+
+else:
+    print('Poor')
+"""
+
+"""student_grade = input("Enter your grade ")
+student_grade = (int(student_grade))
+
+if student_grade == 9:
+    print('Freshman')
+elif student_grade == 10:
+    print('Sophomore')
+elif student_grade == 11:
+    print('Junior')
+elif student_grade == 12:
+    print('Senior')
+else:
+    print('TBD')
+"""
+
+"""
+import random
+fun_fact = random.randint(0, 5)
+
+if fun_fact == 0:
+    print("Flamingos turn pink from eating shrimp.")
+elif fun_fact == 1:
+    print("The only food that doesn\'t spoil is honey.")
+elif fun_fact == 2:
+    print("Shrimp can only swim backwards.")
+elif fun_fact == 3:
+    print("A taste bud\'s life span is about 10 days.")
+elif fun_fact == 4:
+    print("It is impossible to sneeze while sleeping.")
+elif fun_fact == 5:
+    print("It is illegal to sing off-key in North Carolina.")
+"""
